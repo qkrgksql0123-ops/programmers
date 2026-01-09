c1d50ce44c1fbb809affa948d04d0013b9809138
qkrgksql0123-ops <qkrgksql0123@gmail.com>
2026-01-09 16:22:33 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int garo = 2;
+        int num = message.length();
+        int answer = num * 2;
+        return answer;
+    }
+}
