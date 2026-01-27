1062c7349053056511bd90c2e1b5c08bff9fa1b3
qkrgksql0123-ops <qkrgksql0123@gmail.com>
2026-01-27 12:48:29 +0900

    Add files via upload
    회원가입, 로그인

--- Board/src/com/peisia/mysqlconsoleboard/Display.java
+++ Board/src/com/peisia/mysqlconsoleboard/Display.java
@@ -1,22 +1,21 @@
 package com.peisia.mysqlconsoleboard;
 
 public class Display {
-	static private String TITLE_BAR = "🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈"; 
-	static private String TITLE 	= "🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈     게시판     🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈"; 
+	static private String      title = "🔴🟤⚪🔴🟤⚪🔴🟤⚪🔴🟤⚪🔴🟤⚪";
+	static private String title_name = "🔴🟤⚪    게시판 입니다   🔴🟤⚪";
+	static private String Line =       "==============================";
+	static private String Menu =       "1.글보기, 2.글읽기, 3.글수정, 4.글삭제";
 	
-	static private String MAIN_MENU_BAR = "================================================================";
-	static private String MAIN_MENU	 	= "[1]글리스트 [2]글읽기 [3]글쓰기 [4]글삭제 [0]관리자 [e]프로그램종료";
-	
-	static public void showTitle() {
-		System.out.println(TITLE_BAR);
-		System.out.println(TITLE);
-		System.out.println(TITLE_BAR);
+	public static void Title() {
+		System.out.println(title);
+		System.out.println(title_name);
+		System.out.println(title);
 	}
 	
-	static public void showMainMenu() {
-		System.out.println(MAIN_MENU_BAR);
-		System.out.println(MAIN_MENU);
-		System.out.println(MAIN_MENU_BAR);
+	public static void Menu() {
+		System.out.println(Line);
+		System.out.println(Menu);
+		System.out.println(Line);
 		
 	}
 }
--- Board/src/com/peisia/mysqlconsoleboard/ProcBoard.java
+++ Board/src/com/peisia/mysqlconsoleboard/ProcBoard.java
@@ -9,132 +9,158 @@ import java.util.Scanner;
 
 public class ProcBoard {
 	Connection con = null;
-	Statement st = null;
 	ResultSet result = null;
+	Statement st = null;
+	Scanner sc = new Scanner(System.in);
+	int perpage = 3;
 	
-	Scanner sc=new Scanner(System.in);
-	
-	void run() {
-		Display.showTitle();
-		Display.showMainMenu();
-		dbInit();
-		
-		loop:
-		while(true) {
-			System.out.println("명령입력: ");
-			String cmd=sc.next();
-			switch(cmd) {
-			case "1":	//글리스트
-				Count();
-				dbexecutequery();
-				break;
-			case "2":	//글읽기
-				dbread();
-				break;
-			case "3":	//글쓰기	
-				dbwrtie();
-				break;
-			case "4":	//글삭제
-				Dbdel();
-				break;
-			case "0":	//관리자
-				break;
-			case "e":	//프로그램 종료
-				System.out.println("프로그램종료");
-				break loop;
-			}
-		}
-	}
-	
-	private void dbInit() {
+	private void init() {
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_cat", "root", "");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_cat","root","");
 			st = con.createStatement();
-		} catch (Exception e) {
+		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void dbexecutequery() {
-		int Perpage= 3;
-		int start = 0;
-		System.out.println("몇번 페이지");
-		int current = sc.nextInt();
-		start = (current-1)*Perpage;
-		try {
-			result = st.executeQuery("select * from board limit "+start+",3");
-			while(result.next()) {
-				String b_no = result.getString("b_no");
-				String b_id = result.getString("b_id");
-				String b_text = result.getString("b_text");
-				String date_time = result.getString("b_datetime");
-				String sql;
-				sql = String.format("번호 : %s,아이디 :%s, 시간 :%s, 내용 : %s", b_no,b_id,date_time,b_text);
-				System.out.println(sql);
+	 void run() {
+		Display.Title();
+		init();
+		signin();
+		signup();
+		while(true) {
+			Display.Menu();
+			System.out.println("명령 : ");
+			String cmd = sc.next();
+			switch (cmd) {
+			case "1":
+				dbtotal();
+				dblist();
+				break;
+			case "2":
+				dbread();
+				break;
+			case "3":
+				dbupdate();
+				break;
+			case "4":
+				dbdel();
+				break;
+			default:
+				break;
 			}
+			
+		}
+		
+	}
+	private void dblist() {
+		try {
+			System.out.println("몇 페이지 볼거야?");
+			int currentPage = sc.nextInt();
+			int offset = (currentPage-1)*perpage;
+			String sql = String.format("select * from board order by b_no limit %d,%d", offset,perpage);
+			result = st.executeQuery(sql);
+			while(result.next()) {
+				String title = result.getString("b_title");
+				String no = result.getString("b_no");
+				String id = result.getString("b_id");
+				String time = result.getString("b_datetime");
+				System.out.println("번호 :"+no+" 제목 :"+title + "아이디 :"+id+" 시간 :"+time);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	private void dbread() {
+		System.out.println("읽고 싶은 글 번호:");
+			String cmd = sc.next();
+			String sql = String.format("select * from board where b_no='%s'", cmd);
+			System.out.println(sql);
+			try {
+				result = st.executeQuery(sql);
+				while(result.next()) {
+					String no = result.getString("b_no");
+					String text = result.getString("b_text");
+					String comment = result.getString("comment");
+					System.out.println("번호 : "+no+" 내용 : "+text+" 댓글 :"+comment);
+				}
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
+	private void dbupdate() {
+		System.out.println("수정할 글 번호");
+		String no = sc.next();
+		System.out.println("수정 내용 : ");
+		String text = sc.next();
+		String sql = String.format("update member set b_text ='%s' where b_no= '%s'",text,no);
+
+		try {
+			int reuslt = st.executeUpdate(sql);
+			System.out.println("처리된 행 : "+reuslt);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private void dbdel() {
+		System.out.println("삭제할 글 번호");
+		String no = sc.next();
+		String sql = String.format("delete from board where b_no ='%s'",no);
+		try {
+			int result = st.executeUpdate(sql);
+			System.out.println("처리된 행 : "+result);
 		}catch (SQLException e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		
-	
 	}
-	
-	private void dbread() {
-		System.out.println("읽고싶은 글 번호");
-		int user  = sc.nextInt();
+	private void dbtotal() {
 		try {
-			result = st.executeQuery("select * from board where b_no="+user);
+			result = st.executeQuery("select count(*) from board");
 			while(result.next()) {
-				String title = result.getString("b_title");
-				String text = result.getString("b_text");
-				System.out.println("글 제목 :"+title+"내용 : "+text);
+				String count = result.getString("count(*)");
+				System.out.println("글 수 :"+count);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	private void signup() {
+		System.out.println("아이디 : ");
+		String ID = sc.next();
+		System.out.println("비밀번호 : ");
+		String Pw = sc.next();
+		String sql = String.format("insert into member values ('%s','%s')",ID,Pw);
+		try {
+			int result = st.executeUpdate(sql);
+			System.out.println("처리된 행 : "+result);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private void signin() {
+		while(true) {
+		System.out.println("로그인 아이디 : ");
+		String id = sc.next();
+		System.out.println("로그인 비밀번호 : ");
+		String pw = sc.next();
+		String sql = String.format("select * from member where s_id ='%s'and s_pw='%s'", id,pw);
+		try {
+			result = st.executeQuery(sql);
+			if(result.next()) {
+				System.out.println("로그인 성공"+result.getString("s_id")+"님");
+				break;
+				}else {
+					System.out.println("없는 계정 입니다.");
+	
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private void dbwrtie() {
-		System.out.println("글 제목");
-		String title = sc.next();
-		System.out.println("글 아이디");
-		String id = sc.next();
-		System.out.println("글 내용");
-		String text = sc.next();
-		String sql;
-		try {
-		sql = String.format("insert into board values(b_no,'%s','%s',now(),0,'%s')",title,id,text);
-		int result = st.executeUpdate(sql);
-		System.out.println("처리된 행 : "+result );
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
 	}
-	private void Dbdel() {
-		System.out.println("지우고 싶은 글 번호");
-		int delnum = sc.nextInt();
-		try {
-			int result = st.executeUpdate("delete from board where b_no="+delnum);
-			System.out.println("처리된 행 : "+result);
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private void Count() {
-		try {
-			result = st.executeQuery("select count(*) from board");
-			while(result.next()) {
-				String num = result.getString("count(*)");
-				System.out.println("=========================");
-				System.out.println("총 글:"+num);
-				System.out.println("=========================");
-			}}catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
 	}
